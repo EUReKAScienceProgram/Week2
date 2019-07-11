14e38aefb4e08695e32e0867aa2376c9118f3e5f
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-11 10:09:10 -0700

    Create Programmingintro.java

---
+++ Projects/Hawkeye/Programmingintro.java
@@ -0,0 +1,27 @@
+public class Programmingintro {
+
+    public static void main(String[] args) {
+        System.out.println("yEET!!!!!!1!1!!!!!!!!!!1!1!1111!!!!!!");
+
+        int result = myFunction(72, 2345678);
+        System.out.println(result);
+
+        // int thisIsAVariable = 72;
+        // double decimalNumber = 7.22;
+        // boolean trueOrFalse = true;
+        // char aLetter = 'C';
+        // String someText = "Payton is the best!";
+        // String someText2 = "Papyrus is great!";
+
+        // System.out.println(thisIsAVariable);
+        // System.out.println(decimalNumber);
+        // System.out.println(trueOrFalse);
+        // System.out.println(aLetter);
+        // System.out.println(someText);
+        // System.out.println(someText2);
+    }
+    public static int myFunction(int numberOne, int numberTwo){
+        return numberOne * numberTwo;
+
+    }
+}

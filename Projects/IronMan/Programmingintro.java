8d23e815df249e1dce60e8d8bfa75782cd8c5075
EUReKACamper <51757403+EUReKACamper@users.noreply.github.com>
2019-07-12 16:43:06 -0700

    added programming intro to iron man - madi

---
+++ Projects/IronMan/Programmingintro.java
@@ -0,0 +1,25 @@
+public class Programmingintro {
+    
+    public static void main(String[] args) { 
+        // System.out.println("Hello World!");
+        // int thisIsAVariable = 7;
+         int result = myFunction(5, 10);
+         System.out.println(result);
+        // double decimalNumber = 7.12;
+        // boolean trueOrFalse = true;
+        // char aLetter = 'M';
+        // String someText = "Madi Is Awesome!";
+           
+            // System.out.println(thisIsAVariable);
+        // System.out.println(decimalNumber);
+        // System.out.println(trueOrFalse);
+        // System.out.println(aLetter);
+        // System.out.println(someText);
+    }
+
+    public static int myFunction(int numberOne, int numberTwo){
+         return numberOne * numberTwo;
+
+    }
+
+}
